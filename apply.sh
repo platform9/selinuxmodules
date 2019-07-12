b8249b135cb811e5fa6c7f93e53591ea7a08c927
jay vyas <jayunit100.github@gmail.com>
2019-07-12 08:19:17 -0700

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This used to be done in a for loop, but converted to explicit commands so that
+# CS and others can easily uncomment / change files / edit things as needed.  We don't expect more
+# then 10 or 15 policies so, in the long run, there isn't much value in making it dynamic.
+
 pushd policies
 
 # Container certs
@@ -8,8 +12,8 @@ semodule_package -o container_cert.pp -m container_cert.mod
 semodule -i container_cert.pp
 
 # Platform9 user
-checkmodule -M -m -o ./pf9_user.mod ./pf9_user.te
-semodule_package -o pf9_user.pp -m pf9_user.mod
-semodule -i pf9_user.pp
+checkmodule -M -m -o ./pf9.mod ./pf9.te
+semodule_package -o pf9.pp -m pf9.mod
+semodule -i pf9.pp
 
 popd
